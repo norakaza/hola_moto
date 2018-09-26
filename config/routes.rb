5dd25797b0ee24bfd06a22297439f5d3404670f3
Lena Plotnikova <plotman@gmail.com>
2018-09-26 17:26:27 +0200

    Check

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
-  get 'motos', to: 'motos#index'
-  get 'motos/:id', to: 'motos#show'
-  get 'motos/edit'
-  get 'motos/update'
-  get 'motos/new'
-  get 'motos/create'
-  get 'motos/dashboard'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'motos', to: 'motos#index'
+  # get 'motos/:id', to: 'motos#show'
+  # get 'motos/edit'
+  # get 'motos/update'
+  # get 'motos/new'
+  # get 'motos/create'
+  # get 'motos/dashboard'
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :motos, only: [ :index, :show ]
+
+
 end
 
 
