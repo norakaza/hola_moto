24a05e6111ffe334328ae8b1bf05ae45f5125177
Lena Plotnikova <plotman@gmail.com>
2018-09-26 19:35:05 +0200

    Added View for moto#index and Search by brand and name

--- app/controllers/motos_controller.rb
+++ app/controllers/motos_controller.rb
@@ -1,6 +1,15 @@
 class MotosController < ApplicationController
   def index
     @motos = Moto.all
+    if params[:query].present?
+      sql_query = " \
+      motos.brand LIKE :query \
+      OR motos.name LIKE :query \
+    "
+    @motos = @motos.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @motos = Moto.all
+    end
   end
 
   def show
